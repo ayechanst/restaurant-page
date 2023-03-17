70c9f096d3648cd7a65b97c46266a5411d5ee9bd
Aye Chan <ayechan.sant@gmail.com>
2023-03-17 11:14:46 -0700

    testing function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import _ from 'lodash';
+// how do you import files correctly?
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'fartmaster'], ' ');
-
+  element.innerHTML = _.join(['Hello', 'test'], '');
+  // why lodash why the fuck do we need this? Find out
   return element;
 }
 
-document.body.appendChild(component());
+function test() {
+  const content = document.getElementById(content);
+  const poopyFart = document.createElement(div);
+  poopyFart.innerHTML = 'poopy fart';
+  content.appendChild(poopyFart);
+}
 
-console.log('everything is working correctly');
+document.body.appendChild(component());
+document.body.appendChild(test());
