5c81291bf09a3b2bb45978eddc91fc45dc7bffb2
Aye Chan <ayechan.sant@gmail.com>
2023-03-17 05:19:00 -0700

    css added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'fartmaster'], ' ');
 
   return element;
 }
 
 document.body.appendChild(component());
+
+console.log('everything is working correctly');
