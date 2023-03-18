be145801090045f603f1b9aa9f5bdd4c919a9b74
Aye Chan <ayechan.sant@gmail.com>
2023-03-18 15:35:48 -0700

    playing around with webpack

--- bareBones.js
+++ bareBones.js
@@ -1,1 +1,6 @@
-export default function curryImage() {}
+export default function title() {
+  const title = document.createElement('h1');
+  title.innerHTML = "Aye Chan's Asian Appetizers";
+  console.log('the import has worked');
+  return title;
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import _ from 'lodash';
-// how do you import files correctly?
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'test'], '');
-  // why lodash why the fuck do we need this? Find out
-  return element;
-}
-
-function test() {
-  const content = document.getElementById(content);
-  const poopyFart = document.createElement(div);
-  poopyFart.innerHTML = 'poopy fart';
-  content.appendChild(poopyFart);
-}
+import title from 'bareBones.js';
 
 document.body.appendChild(component());
-document.body.appendChild(test());
+title();
+console.log('index.js is working');
