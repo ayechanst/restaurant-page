7f2ff36f376984ef40f8d8d476fab0f57b71ec85
Aye Chan <ayechan.sant@gmail.com>
2023-03-19 06:42:35 -0700

    sorting things out

--- bareBones.js
+++
@@ -1,6 +0,0 @@
-export default function title() {
-  const title = document.createElement('h1');
-  title.innerHTML = "Aye Chan's Asian Appetizers";
-  console.log('the import has worked');
-  return title;
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash';
-import title from 'bareBones.js';
-
-document.body.appendChild(component());
-title();
-console.log('index.js is working');
