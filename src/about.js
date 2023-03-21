bcf0230e0b2de795a7bd6edc8e1faca1f33dff7c
Aye Chan <ayechan.sant@gmail.com>
2023-03-21 09:59:20 -0700

    done

--- src/about.js
+++ src/about.js
@@ -1,14 +1,29 @@
+import { makeHome } from './home';
+
 export function makeAboutPage() {
-  const body = document.getElementById('body');
-  const aboutCard = document.createElement('div');
-  const aboutCardContent = document.createElement('div');
-  aboutCard.className = 'about-card';
-  aboutCardContent.innerHTML =
-    'The Feral Fox is a restaurant for humans commited to staying true to the fox way of life.\
-     Everything served here is authentic fox food that foxes would eat. The food is taken from \
-     the wild and modified by our fox chefs to perfection. The restaurant serves special food \
-     out of most humans comfort zones! Please be open minded and appreciate the unique flavors \
-     found in no other cuisine in the world. Fox approved, certified, and ran. ';
-  aboutCard.appendChild(aboutCardContent);
-  body.appendChild(aboutCard);
+  makeHome();
+  const textContainer = document.querySelector('.text-container');
+  textContainer.innerHTML = '';
+  // append things to the text container
+  const heading = document.createElement('h2');
+  const aboutText = document.createElement('div');
+
+  heading.innerHTML = 'About Us';
+  aboutText.innerHTML = `
+  This is a fox family owned restuarant owened and operated by
+  foxes. This amazing restaurant would not be possible without
+  the help of one human however. Due to lack of opposable thumbs,
+  a web designer has been employed by the owner. The web designer
+  is paid in free rotten flesh. 
+  
+  The restuarnt was founded in 2012 in Silicon Valley as a trendy
+  "out there" sort of dining experience. The cheap food also made
+  it accessible to all sorts of people, and in 2018 the restaurant
+  branched into its large chain serving millions worldwide. Our 
+  pledge is to serve as authentic as possible fox food without 
+  any compromises on quality.
+  `;
+
+  textContainer.appendChild(heading);
+  textContainer.appendChild(aboutText);
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import { makeHome } from './home';
 import { makeSkeleton } from './skeleton';
 
 if (content.innerHTML == false) {
   makeSkeleton();
+  makeHome();
 }
