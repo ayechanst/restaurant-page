326eadfa4f736ddbd6d637068b91a57ffb29d3ba
Aye Chan <ayechan.sant@gmail.com>
2023-03-20 17:31:13 -0700

    making the menu look nice with reusable cards

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,16 @@
 export const foxMenuItems = [
-  ['Burnt Bird', 0.5, 'Crow struck by lightning seasoned with dirt and gravel'],
-  ['Rotting Roadkill', 1, 'Sun dried flesh with patches of fur and an antler'],
+  [
+    'Burnt Bird',
+    0.5,
+    'Crow struck by lightning seasoned with dirt and gravel',
+    'burnt-crow.jpg',
+  ],
+  [
+    'Rotting Roadkill',
+    1,
+    'Sun dried flesh with patches of fur and an antler',
+    'decaying-dog.jpg',
+  ],
   [
     'Decaying Dog',
     4,
@@ -22,25 +32,31 @@ export const foxMenuItems = [
 const humanMenuItems = ['rice', 'steak', 'sparkling water'];
 
 function makeCard() {
-  // declare variables, and this function should make card objects with its called
   const card = document.createElement('div');
-  const cardContent = document.createElement('div');
   card.className = 'card';
-  cardContent.className = 'card-content';
-  card.appendChild(cardContent);
   return card;
 }
 
 function fillCards(menuItems) {
   const cardArray = [];
+
   menuItems.forEach((item) => {
     let card = makeCard();
     const dish = document.createElement('div');
     const price = document.createElement('div');
     const description = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const imgWrapper = document.createElement('div');
+
     dish.innerHTML = `${item[0]}`;
     price.innerHTML = `$${item[1]}`;
     description.innerHTML = item[2];
+    imgContainer.style.backgroundImage = `url('../images/${item[3]}')`;
+    imgContainer.className = 'card-image-container';
+    imgWrapper.className = 'image-wrapper';
+
+    imgWrapper.appendChild(imgContainer);
+    card.appendChild(imgWrapper);
     card.appendChild(dish);
     card.appendChild(price);
     card.appendChild(description);
@@ -57,3 +73,5 @@ export function makeMenu() {
   body.appendChild(menuCardContainer);
   menuCardContainer.className = 'menu-card-container';
 }
+
+// fix the cards and make them look nice
